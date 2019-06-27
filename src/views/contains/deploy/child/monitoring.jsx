02c5407c73444cb5348d63f09e4cbce23888924e
unknown <zhenzeng@issamwayodc.com>
2019-06-27 21:09:11 +0800

    视频、地图

--- src/views/contains/deploy/child/monitoring.jsx
+++ src/views/contains/deploy/child/monitoring.jsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 import './monitoring.css'; // css样式
 import { Tree, Slider } from 'antd'
 const { TreeNode } = Tree;
+const iconCameraUrl = require( '../../../../common/monitor-img/icon_shexiang.png')
+const iconShebeiUrl = require( '../../../../common/monitor-img/icon_shebei.png')
+const linjinPhoto = require( '../../../../common/monitor-img/shipin.png')
+const mainSrc = require('./rpi_car.MP4')
 
 class componentName extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            currRoom: null,
+            videoRef: [],
+            status: false,
+            volume: 100,
+            volumeRange: false,
+            map: null,
+            deviceList: [],
             btn: true,
             cameraNo: 'dzm-001-03',
             control: '1',
@@ -116,10 +127,16 @@ class componentName extends Component {
                             "key":"0-0-0-0-0",
                             "children":[{
                                 "title":"设备类型-编号01",
-                                "key":"0-0-0-0-0-0"
+                                "key":"0-0-0-0-0-0",
+                                "type": "camera",
+                                "lng":39.90,
+                                "lat":116.40
                             },{
                                 "title":"设备类型-编号02",
-                                "key":"0-0-0-0-0-1"
+                                "key":"0-0-0-0-0-1",
+                                "type": "shebei",
+                                "lng":40.00,
+                                "lat":116.50
                             },{
                                 "title":"人员A",
                                 "key":"0-0-0-0-0-2"
@@ -129,10 +146,16 @@ class componentName extends Component {
                             "key":"0-0-0-0-1",
                             "children":[{
                                 "title":"设备类型-编号01",
-                                "key":"0-0-0-0-1-0"
+                                "key":"0-0-0-0-1-0",
+                                "type": "camera",
+                                "lng":39.91,
+                                "lat":116.51
                             },{
                                 "title":"设备类型-编号02",
-                                "key":"0-0-0-0-1-1"
+                                "key":"0-0-0-0-1-1",
+                                "type": "shebei",
+                                "lng":40.92,
+                                "lat":116.52
                             },{
                                 "title":"人员A",
                                 "key":"0-0-0-0-1-2"
@@ -146,10 +169,16 @@ class componentName extends Component {
                             "key":"0-0-0-0-0",
                             "children":[{
                                 "title":"设备类型-编号01",
-                                "key":"0-0-0-0-0-0"
+                                "key":"0-0-0-0-0-0",
+                                "type": "shebei",
+                                "lng":39.93,
+                                "lat":116.53
                             },{
                                 "title":"设备类型-编号02",
-                                "key":"0-0-0-0-0-1"
+                                "key":"0-0-0-0-0-1",
+                                "type": "camera",
+                                "lng":39.70,
+                                "lat":116.20
                             },{
                                 "title":"人员A",
                                 "key":"0-0-0-0-0-2"
@@ -159,10 +188,16 @@ class componentName extends Component {
                             "key":"0-0-0-0-1",
                             "children":[{
                                 "title":"设备类型-编号01",
-                                "key":"0-0-0-0-1-0"
+                                "key":"0-0-0-0-1-0",
+                                "type": "shebei",
+                                "lng":39.80,
+                                "lat":116.30
                             },{
                                 "title":"设备类型-编号02",
-                                "key":"0-0-0-0-1-1"
+                                "key":"0-0-0-0-1-1",
+                                "type": "shebei",
+                                "lng":39.70,
+                                "lat":116.20
                             },{
                                 "title":"人员A",
                                 "key":"0-0-0-0-1-2"
@@ -180,10 +215,16 @@ class componentName extends Component {
                             "key":"0-0-1-0-0",
                             "children":[{
                                 "title":"设备类型-编号01",
-                                "key":"0-0-1-0-0-0"
+                                "key":"0-0-1-0-0-0",
+                                "type": "camera",
+                                "lng":39.60,
+                                "lat":116.10
                             },{
                                 "title":"设备类型-编号02",
-                                "key":"0-0-1-0-0-1"
+                                "key":"0-0-1-0-0-1",
+                                "type": "camera",
+                                "lng":39.50,
+                                "lat":116.00
                             },{
                                 "title":"人员A",
                                 "key":"0-0-1-0-0-2"
@@ -193,10 +234,14 @@ class componentName extends Component {
                             "key":"0-0-1-0-1",
                             "children":[{
                                 "title":"设备类型-编号01",
-                                "key":"0-0-1-0-1-0"
+                                "key":"0-0-1-0-1-0",
+                                "lng":36.40,
+                                "lat":113.40
                             },{
                                 "title":"设备类型-编号02",
-                                "key":"0-0-1-0-1-1"
+                                "key":"0-0-1-0-1-1",
+                                "lng":35.90,
+                                "lat":112.90
                             },{
                                 "title":"人员A",
                                 "key":"0-0-1-0-1-2"
@@ -210,10 +255,14 @@ class componentName extends Component {
                             "key":"0-0-1-0-0",
                             "children":[{
                                 "title":"设备类型-编号01",
-                                "key":"0-0-1-0-0-0"
+                                "key":"0-0-1-0-0-0",
+                                "lng":41.90,
+                                "lat":117.90
                             },{
                                 "title":"设备类型-编号02",
-                                "key":"0-0-1-0-0-1"
+                                "key":"0-0-1-0-0-1",
+                                "lng":42.40,
+                                "lat":118.40
                             },{
                                 "title":"人员A",
                                 "key":"0-0-1-0-0-2"
@@ -223,10 +272,14 @@ class componentName extends Component {
                             "key":"0-0-1-0-1",
                             "children":[{
                                 "title":"设备类型-编号01",
-                                "key":"0-0-1-0-1-0"
+                                "key":"0-0-1-0-1-0",
+                                "lng":42.90,
+                                "lat":118.90
                             },{
                                 "title":"设备类型-编号02",
-                                "key":"0-0-1-0-1-1"
+                                "key":"0-0-1-0-1-1",
+                                "lng":43.40,
+                                "lat":119.40
                             },{
                                 "title":"人员A",
                                 "key":"0-0-1-0-1-2"
@@ -235,15 +288,37 @@ class componentName extends Component {
                     }]
                 }]
             }],
-            expandedKeys: ['0-0', '0-0-0', '0-0-0-0'],
-            monitorList: [{}]
+            expandedKeys: ['0-0', '0-0-0', '0-0-0-0']
         }
     }
 
     componentDidMount() {
-        // BM.Config.HTTP_URL = 'http://www.bigemap.com:9000';
+        window.BM.Config.HTTP_URL = 'http://www.bigemap.com:9000';
         // 在ID为map的元素中实例化一个地图，并设置地图的ID号为 bigemap.googlemap-streets，ID号程序自动生成，无需手动配置，并设置地图的投影为百度地图 ，中心点，默认的级别和显示级别控件
-        // var map = BM.map('map', 'bigemap.googlemap-streets', { center: [39.9023974684,116.4056851466], zoom: 10, zoomControl: true });
+        var map = window.BM.map('map', 'bigemap.googlemap-satellite', { center: [39.9023974684,116.4056851466], zoom: 10, zoomControl: true });
+        const { gData } = this.state;
+        const list = [];
+        this.findDevice(gData, list);
+        var video = this.refs.mainVideo;
+        const volume = video.volume * 100;
+        console.log(list)
+        this.setState({
+            map,
+            deviceList: list,
+            volume
+        })
+        this.setMarker();
+
+    }
+
+    findDevice = (data, deviceList) => {
+        data.map(item=>{
+            if(item.children){
+                this.findDevice(item.children, deviceList)
+            }else{
+                deviceList.push(item)
+            }
+        })
     }
 
     expand() {
@@ -259,8 +334,166 @@ class componentName extends Component {
         })
     }
 
+    stopVoip = () => {
+        const { videoRef } = this.state;
+        videoRef.map(item=>{
+            item.srcObject = null;
+            item.load();
+        })
+        this.setState({
+            currRoom: null
+        })
+    }
+
+    voipSetStream = (streamObj) => {
+        const { videoRef } = this.state;
+        videoRef.map(item=>{
+            if(!item.srcObject){
+                item.srcObject = streamObj;
+                item.play();
+            }
+        })
+    }
+
+    voipCallBack = (data, status, oper) => {
+        var thisRoom = data.obj;
+        switch (status){
+            case "connect success":
+                //连接成功
+                thisRoom.createStream();
+                break;
+            case "connect failed":
+            case "connect closed":
+                this.stopVoip();
+                //连接失败
+            break;
+            case "onWebrtcMessage":
+                //视频接口消息
+                switch(data.type)
+                {
+                    case "streamCreated":
+                        //本地视频流创建，data.status == "success"为成功
+                        if (data.status === "success") {
+                            this.voipSetStream(data.streamObj);
+                            thisRoom.joinRoom();
+                        }
+                        else {
+                            // voipConnectDlg.dialog("close");
+                            alert("获取摄像头视频失败！请检查摄像头设备是否接入！error:" + data.error);
+                        }
+                        break;
+                    case "voipStreamReady":
+                        //远端视频流准备，data.status == "success"为成功
+                        //可以使用data.streamObj对象
+					    this.voipSetStream(data.streamObj);
+                        break;
+                }
+                break;
+            case "onVoipMessage":
+                switch(data.type)
+                {
+                    case "voipRefuse":
+                    //对方拒绝了通话
+                    break;
+                    case "voipHangup":
+                    //对方挂断了通话
+                    break;
+                    case "voipConnect":
+                    //voip接通
+                    break;
+                    case "voipBusy":
+                    //对方正忙
+                    break;
+                    case "voipSingleMsg":
+                    //收到voip消息
+                    break;
+                }
+                break;
+        }
+    }
+
+    setMarker = () => {
+        const { deviceList } = this.state;
+        console.log(deviceList)
+        const _this = this;
+        deviceList.slice(0,2).map(item=>{
+            let marker, currRoom;
+            const { map } = this.state;
+            if(item.type === "camera"){
+                marker = window.BM.marker(window.BM.latLng(item.lng,item.lat),{icon:window.BM.icon({iconUrl:iconCameraUrl})}).addTo(map);
+            }else{
+                marker = window.BM.marker(window.BM.latLng(item.lng,item.lat),{icon:window.BM.icon({iconUrl:iconShebeiUrl})}).addTo(map);
+            }
+            marker.on("click", function(e){
+                currRoom = window.StarRtc.Instance.getVoipRoomSDK("call", _this.voipCallBack, { "roomInfo": { "targetId": item.key } });
+                currRoom.sigConnect();
+                _this.setState({
+                    currRoom: currRoom
+                });
+            })
+        })
+    }
+    
+    onSelect = (selectedKey, info) => {
+        console.log('selected', selectedKey, info);
+        console.log(this)
+        const list = [];
+        this.findDevice(info.selectedNodes, list);
+        this.setState({
+            deviceList: list
+        })
+        this.setMarker();
+    }
+
+    handleVideoRef = (target) => {
+        const { videoRef } = this.state;
+        videoRef.push(target);
+    }
+
+    playVideo(index) {
+        // $("#mainVideo")[0].srcObject = this.state.videoRef[index].srcObject;
+        // this.refs.mainVideo.src = "https://github.com/starrtc/iot_car_demo/raw/master/rpi_car.MP4";
+    }
+
+    vidplay = () => {
+        var video = this.refs.mainVideo;
+        const { status } = this.state;
+        if(video.paused) {
+            video.play();
+            this.setState({
+                status: !status
+            })
+        }else{
+            video.pause();
+            this.setState({
+                status: !status
+            })
+        }
+    }
+    
+    skip = (value) => {
+        var video = this.refs.mainVideo;
+        // var video = document.getElementById("mainVideo");
+        video.currentTime += value;
+    }
+
+    volume = () => {
+        const { volumeRange } = this.state;
+        this.setState({
+            volumeRange: !volumeRange
+        })
+    }
+
+    handleVolume = (value) => {
+        var video = this.refs.mainVideo;
+        video.volume = value/100;
+        this.setState({
+            volume: value/100
+        })
+    }
+
     render () {
-        const { gData, btn, cameraNo, commentList, monitorList, nearbyList, control, 
+        const { gData, btn, cameraNo, commentList, nearbyList, control, status, volume, volumeRange,
             luminosity, transparency, tone, saturation, frameRate, definition } = this.state
 
         const loop = data =>
@@ -283,6 +516,7 @@ class componentName extends Component {
                             <Tree
                                 className="draggable-tree"
                                 defaultExpandedKeys={this.state.expandedKeys}
+                                onSelect={this.onSelect}
                             >
                                 {loop(gData)}
                             </Tree>
@@ -292,28 +526,26 @@ class componentName extends Component {
                 </div>
                 <div className="monitor-middle">
                     <ul className="monitor-list">
-                        {
-                            monitorList.map(item=>{
-                                return (
-                                    <li>
-                                        <video width="260" height="160" autoPlay>
-                                            <source src={item.src} type="video/mp4" />
-                                            您的浏览器不支持 HTML5 video 标签。
-                                        </video>
-                                    </li>
-                                )
-                            })
-                        }
+                        <li onClick={this.playVideo.bind(this,1)}><video width="260" height="160" ref={this.handleVideoRef}></video></li>
+                        <li onClick={this.playVideo.bind(this,1)}><video width="260" height="160" ref={this.handleVideoRef}></video></li>
+                        <li onClick={this.playVideo.bind(this,1)}><video width="260" height="160" ref={this.handleVideoRef}></video></li>
+                        <li onClick={this.playVideo.bind(this,1)}><video width="260" height="160" ref={this.handleVideoRef}></video></li>
                     </ul>
                 </div>
                 <div className={`btn ${ btn ? 'shouqi' : 'tankai' }`} onClick={this.expand.bind(this)}></div>
                 <div className={`monitor-right ${btn ? '': 'hide'}`} >
                     集中监控
                     <div className="content">
-                        <video width="704" height="400" autoPlay>
-                            <source src='' type="video/mp4" />
-                            您的浏览器不支持 HTML5 video 标签。
-                        </video>
+                        <video id="mainVideo" width="704" height="400" ref="mainVideo" autoPlay src={mainSrc}></video>
+                        <div className="buttonDiv">
+                            <button id="rew" onClick={this.skip.bind(this,-10)}></button>
+                            <button id="play" className={`${status?'zanting':''}`} onClick={this.vidplay.bind(this)}></button>
+                            <button id="fastFwd" onClick={this.skip.bind(this,10)}></button>
+                            <button id="volume" onClick={this.volume.bind(this)}></button>
+                            <Slider vertical disabled={false} defaultValue={volume} onChange={this.handleVolume}
+                                className={`volumeSlider ${volumeRange?'':'hide'}`}
+                            />
+                        </div>
                         <div className="video-right">
                             <p>摄像头编号：{cameraNo}</p>
                             <ul className="video-control">
@@ -372,10 +604,11 @@ class componentName extends Component {
                                         }else {
                                             return (
                                                 <li>
-                                                    <video width="79" height="60" autoPlay>
+                                                    {/* <video width="79" height="60" autoPlay>
                                                         <source src='' type="video/mp4" />
                                                         您的浏览器不支持 HTML5 video 标签。
-                                                    </video>
+                                                    </video> */}
+                                                    <img src={linjinPhoto} width="79" height="60" onClick={this.playVideo} />
                                                     <ul className={`markList ${(index+1)%3 ? '' : 'mr0'}`}>
                                                             <li className="title">标注：</li>
                                                             {
